82dee65ebeec2e798f15396d8a075d760c0ef98d
Gareth Campbell <gareth.cc@gmail.com>
2021-11-15 10:40:31 -0500

    Add new user to adobe group for creative suite access

---
+++ powershellScripts/BuildingBlocks/NewAdobeUsers.ps1
@@ -0,0 +1,4 @@
+﻿
+#Adds a single user to the AdobeUsers user group in order to give them adobe privleges. 
+$User = read-host "Who is the user?"
+Add-ADGroupMember -Identity AdobeUsers -Members $User
